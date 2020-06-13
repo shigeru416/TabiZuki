668fa008cf66a211b2cca0d2575d80218c1271ef
松崎匡浩 <game.1andwork.1etc@gmail.com>
2020-06-13 18:20:49 +0900

    [add]views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+	root "homes#home"
+	get "/about", to:"homes#about"
+	devise_for :users
+	resources :posts, only:[:new, :create, :index, :show] do
+		resource :post_comments, only: [:create, :destroy]
+		resource :favorites, only: [:create, :destroy]
+		resource :wannago_lists, only: [:create, :destroy]
+	end
+
+	devise_for :admins
+	namespace :admins do
+		root "homes#home"
+		resources :categories, only:[:index, :create, :destroy]
+	end
 end
