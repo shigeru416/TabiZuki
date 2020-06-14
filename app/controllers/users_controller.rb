0220336e004630ffe6f973bd057b2b98b56b40d9
松崎匡浩 <game.1andwork.1etc@gmail.com>
2020-06-14 23:56:04 +0900

    [start]making_timeline

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,14 +14,6 @@ class UsersController < ApplicationController
 		render :show
 	end
 
-	def follows
-
-	end
-
-	def followers
-
-	end
-
 	private
 	def user_params
 		params.require(:user).permit(:name, :image, :introduction)
