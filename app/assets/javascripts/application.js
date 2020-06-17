3a4daace4c3da03e72af28fbcdad13fd3de602ff
松崎匡浩 <game.1andwork.1etc@gmail.com>
2020-06-17 23:19:30 +0900

    [edit]whole_views_adnd_fixed_appscss

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -42,22 +42,20 @@ $(document).ready(function(){
     pause: 6000,           // 自動スライドの待ち時間
     maxSlides: 1,          // 一度に表示させる最大数
     slideWidth: 700,      // 各スライドの幅
-	randomStart: true,     // 最初に表示するスライドをランダムに設定
+	  randomStart: true,     // 最初に表示するスライドをランダムに設定
     autoHover: true,       // ホバー時に自動スライドを停止
-    mode: 'fade',          
+    mode: 'fade',
     controls: false        // 矢印消す
   });
 });
 
-/*投稿詳細画面スライダー*/
 $(document).ready(function(){
-  $('.bxslider2').bxSlider({
-    auto: true,            // 自動スライド
-    speed: 900,           // スライドするスピード
+  $('.bxslider1').bxSlider({
+    auto: false,            // 自動スライド
+    speed: 1000,           // スライドするスピード
     moveSlides: 1,         // 移動するスライド数
-    pause: 3000,           // 自動スライドの待ち時間
+    pause: 6000,           // 自動スライドの待ち時間
     maxSlides: 1,          // 一度に表示させる最大数
-    slideWidth: 1000,      // 各スライドの幅
     randomStart: true,     // 最初に表示するスライドをランダムに設定
     autoHover: true,       // ホバー時に自動スライドを停止
     mode: 'fade',
