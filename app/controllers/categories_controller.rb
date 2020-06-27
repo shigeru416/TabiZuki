d97bc874243d31076fb63f03393ab37034f62ed9
松崎匡浩 <game.1andwork.1etc@gmail.com>
2020-06-27 19:36:45 +0900

    [edit]views

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,6 +3,6 @@ class CategoriesController < ApplicationController
 	def show
 		@categories = Category.all
 		@category = Category.find(params[:id])
-		@posts = Category.find(params[:id]).posts.order(favorite_count: :desc).page(params[:page])
+		@posts = Category.find(params[:id]).posts.order(created_at: :desc).page(params[:page])
 	end
 end
