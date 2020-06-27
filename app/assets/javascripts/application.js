35e225f9a88575d535b881bb857496e5f7aaddde
松崎匡浩 <game.1andwork.1etc@gmail.com>
2020-06-28 03:22:05 +0900

    [edit]views

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -56,7 +56,6 @@ $(document).ready(function(){
     moveSlides: 1,         // 移動するスライド数
     pause: 6000,           // 自動スライドの待ち時間
     maxSlides: 1,          // 一度に表示させる最大数
-    randomStart: true,     // 最初に表示するスライドをランダムに設定
     autoHover: true,       // ホバー時に自動スライドを停止
     mode: 'fade',
   });
