5b08b885d9866d555fecc805b83f4a041d3ec9a5
松崎匡浩 <game.1andwork.1etc@gmail.com>
2020-06-30 00:07:45 +0900

    [edit]views

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -41,23 +41,22 @@ $(document).ready(function(){
     moveSlides: 1,         // 移動するスライド数
     pause: 6000,           // 自動スライドの待ち時間
     maxSlides: 1,          // 一度に表示させる最大数
-    slideWidth: 700,      // 各スライドの幅
+    slideWidth: 700,       // 各スライドの幅
 	  randomStart: true,     // 最初に表示するスライドをランダムに設定
     autoHover: true,       // ホバー時に自動スライドを停止
-    mode: 'fade',
+    mode: 'fade',          // スライド切り替わり時の変化
     controls: false        // 矢印消す
   });
 });
 
 $(document).ready(function(){
   $('.bxslider1').bxSlider({
-    auto: false,            // 自動スライド
+    auto: false,           // 自動スライド
     speed: 1000,           // スライドするスピード
     moveSlides: 1,         // 移動するスライド数
-    pause: 6000,           // 自動スライドの待ち時間
     maxSlides: 1,          // 一度に表示させる最大数
-    autoHover: true,       // ホバー時に自動スライドを停止
-    mode: 'fade',
+    mode: 'fade',          // スライド切り替わり時の変化
+    adaptiveHeight:true
   });
 });
 
