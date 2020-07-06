73c07c8a164753fcbfe3b6e8f834d8bf73114ff3
松崎匡浩 <game.1andwork.1etc@gmail.com>
2020-07-06 23:02:49 +0900

    [edit]views_vision_language

--- app/controllers/vision_images_controller.rb
+++ app/controllers/vision_images_controller.rb
@@ -6,22 +6,38 @@ class VisionImagesController < ApplicationController
 
 	def create
 		@vision_image = VisionImage.new(vision_images_params)
-		@vision_image.save
-		tags = Vision.get_image_data(@vision_image.image,"label")
-		tags.each do |tag|
-			@vision_image.vision_tags.create(name: tag[0], score: tag[1])
+		if @vision_image.save
+			tags = Vision.get_image_data(@vision_image.image,"label")
+			tags.each do |tag|
+				@vision_image.vision_tags.create(name: tag[0], score: tag[1])
+			end
+			#landmarks = Vision.get_image_data(@vision_image.image,"landmark")
+			#landmarks.each do |landmark|
+				#a = @vision_image.vision_landmarks.create(name: landmark[0], score: landmark[1], location: landmark[2])
+				#latlng = a.location.scan(/[+\-]?\d{1,3}.\d{1,14}/)
+				#lat = latlng[0]
+				#lng = latlng[1]
+				#a.latitude = lat
+				#a.longitude = lng
+				#a.save
+			#end
+			if landmarks = Vision.get_image_data(@vision_image.image,"landmark").present?
+				landmarks = Vision.get_image_data(@vision_image.image,"landmark").flatten
+				@vision_image.vision_landmarks.create(
+					name: landmarks[0],
+					score: landmarks[1],
+					location: landmarks[2],
+					latitude: landmarks[2]["latLng"]["latitude"],
+					longitude: landmarks[2]["latLng"]["longitude"]
+				)
+
+				redirect_to vision_image_path(@vision_image)
+			else
+				redirect_to vision_image_path(@vision_image)
+			end
+		else
+			render :new
 		end
-		landmarks = Vision.get_image_data(@vision_image.image,"landmark")
-		landmarks.each do |landmark|
-			a = @vision_image.vision_landmarks.create(name: landmark[0], score: landmark[1], location: landmark[2])
-			latlng = a.location.scan(/[+\-]?\d{1,3}.\d{1,14}/)
-			lat = latlng[0]
-			lng = latlng[1]
-			a.latitude = lat
-			a.longitude = lng
-			a.save
-		end
-		redirect_to vision_image_path(@vision_image)
 	end
 
 	def show
--- app/models/vision_image.rb
+++ app/models/vision_image.rb
@@ -4,4 +4,5 @@ class VisionImage < ApplicationRecord
 	has_many :vision_landmarks
 
 	attachment :image
+	validates :image, presence: true
 end
