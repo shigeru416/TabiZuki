16d1df962885cccf71b5cf3b2ed8b3f91a27c7ca
松崎匡浩 <game.1andwork.1etc@gmail.com>
2020-07-19 21:02:11 +0900

    [edit]rspec

--- spec/requests/categories_request_spec.rb
+++ spec/requests/categories_request_spec.rb
@@ -2,7 +2,15 @@ require 'rails_helper'
 
 RSpec.describe "Categories", type: :request do
 
-  describe "GET /show" do
+  describe "カテゴリーページ" do
+    context "カテゴリーページが表示される" do
+        before do
+          get admins_categories_path
+        end
+        it "リクエストは200 OKとなること" do
+          expect(response.status).to eq 200
+        end
+    end
   end
 
 end
